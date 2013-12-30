fa4d65677c1602130a309daaf73c3b045c3a7af1
Alex <aleksej@gordejev.lv>
2013-12-31 01:28:53 +0200

    Add session store

--- config/session.js
+++ config/session.js
@@ -8,15 +8,26 @@
  **/
  
 var config = require('./configuration');
+var CaminteStore = require('connect-caminte');
+var database = require('./database' ).db;
 
 module.exports = function (app,express) {
+    var sessionStore = CaminteStore(express);
     app.configure(function () {
         app.use(express.session({
             cookie: {
                 maxAge: config.session.maxAge
             },
             key: config.session.key,
-            secret: config.session.secret
+            secret: config.session.secret,
+            store: new sessionStore({
+                driver: database.driver,
+                collection: 'session',
+                db: database,
+                secret: config.session.secret,
+                maxAge: config.session.maxAge,
+                clear_interval: config.session.clear_interval
+            })
         }));
     });
 };
